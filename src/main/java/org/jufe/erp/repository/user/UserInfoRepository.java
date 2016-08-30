4fe47c35d7db960e423db8baef0c0007ca99de6e
Raomuyang <raomengnan@qq.com>
2016-08-30 16:20:36 +0800

    Add UserInfoRepository

---
+++ src/main/java/org/jufe/erp/repository/user/UserInfoRepository.java
@@ -0,0 +1,48 @@
+package org.jufe.erp.repository.user;
+
+import org.bson.types.ObjectId;
+import org.jufe.erp.entity.UserInfo;
+import org.jufe.erp.repository.BaseRepository;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by Raomengnan on 2016/8/30.
+ */
+@Repository
+public class UserInfoRepository extends BaseRepository<UserInfo>{
+
+    public UserInfo findByUserId(String userId){
+        return super.findOne(new Query(new Criteria("userId").is(userId)));
+    }
+
+    /**
+     * 只要userInfo对象中的id能顺利转为ObjectId，就基本不会有外部侵入的问题
+     * @param userInfo
+     * @return
+     */
+    public boolean update(UserInfo userInfo){
+        try {
+            ObjectId id = new ObjectId(userInfo.getId());
+            return super.save(userInfo);//如果没有信息，则创建
+        }catch (Exception e){
+            logger.error(e.toString());
+            return false;
+        }
+
+    }
+
+    /**
+     * 清除详细信息
+     * 若出错，则返回的list为null
+     * @param userId
+     * @return
+     */
+    public List<UserInfo> deleteByUserId(String userId){
+        return super.delete(new Query(new Criteria("userId").is(userId)));
+    }
+
+}
