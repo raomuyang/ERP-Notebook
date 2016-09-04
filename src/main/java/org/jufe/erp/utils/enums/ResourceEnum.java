b92ba728907f26cbde0748ccfb9ee88a88389cec
Raomuyang <raomengnan@qq.com>
2016-09-04 21:55:48 +0800

    Modified some tools

--- src/main/java/org/jufe/erp/utils/enums/ResourceEnum.java
+++ src/main/java/org/jufe/erp/utils/enums/ResourceEnum.java
@@ -4,10 +4,10 @@ package org.jufe.erp.utils.enums;
  * Created by Raomengnan on 2016/9/4.
  */
 public enum ResourceEnum {
-    NEWSIMAGE("/news"),
-    PHOTO("/photos"),
-    TIMESHAFT("/timeshaft"),
-    VIDEO("/videos");
+    NEWSIMAGE("news"),
+    PHOTO("photos"),
+    TIMESHAFT("timeshaft"),
+    VIDEO("videos");
 
     private String path;
     ResourceEnum(String path){
