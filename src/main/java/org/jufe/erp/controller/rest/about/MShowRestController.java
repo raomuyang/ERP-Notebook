946e9bb67bffc24920ec5eb25ac2563907875596
Raomengnan <raomengnan@foxmail.com>
2016-09-12 16:01:29 +0800

    MODIFY

--- src/main/java/org/jufe/erp/controller/rest/about/MShowRestController.java
+++ src/main/java/org/jufe/erp/controller/rest/about/MShowRestController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * Created by raomengnan on 16-9-12.
