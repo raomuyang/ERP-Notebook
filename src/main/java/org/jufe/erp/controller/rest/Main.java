982977aad8cfe17a3801cd08b8e18698e8cbf782
raomengnan <raomengnan@qq.com>
2016-11-14 20:19:49 +0800

    更新API文档

--- src/main/java/org/jufe/erp/controller/rest/Main.java
+++ src/main/java/org/jufe/erp/controller/rest/Main.java
@@ -2,13 +2,14 @@ package org.jufe.erp.controller.rest;
 
 import org.jufe.erp.repository.QOSComponent;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Raomengnan on 2016/11/14.
  */
-@Controller
+@RestController
+@RequestMapping("/rest")
 public class Main {
     @Autowired
     private QOSComponent qosComponent;
